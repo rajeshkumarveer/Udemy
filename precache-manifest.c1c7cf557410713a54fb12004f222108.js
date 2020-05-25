b90112e627cdd5b96c353ec008a5913b34bcb779
Rajesh <rajeshkumarveer2305@gmail.com>
2020-05-25 18:45:43 +0530

    Updates

---
+++ precache-manifest.c1c7cf557410713a54fb12004f222108.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c742503d6a1fbea7bf9c37b59f43f18b",
+    "url": "/Udemy/index.html"
+  },
+  {
+    "revision": "542c88c9272fb6b65ec5",
+    "url": "/Udemy/static/css/main.65e78204.chunk.css"
+  },
+  {
+    "revision": "e275e21371314ef311ad",
+    "url": "/Udemy/static/js/2.da6c00a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Udemy/static/js/2.da6c00a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542c88c9272fb6b65ec5",
+    "url": "/Udemy/static/js/main.1e95049b.chunk.js"
+  },
+  {
+    "revision": "3de6f13f2a308b4bdce5",
+    "url": "/Udemy/static/js/runtime-main.5555b314.js"
+  },
+  {
+    "revision": "7dc11bef50f465abd9c5964ddac72579",
+    "url": "/Udemy/static/media/udemy_home.7dc11bef.jpg"
+  }
+]);
